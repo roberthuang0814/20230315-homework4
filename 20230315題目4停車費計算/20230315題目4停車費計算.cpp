5492d858c43e112a30954b91e173daf9c482f6d9
rober <rober@LAPTOP-9BJTS009>
2023-03-15 20:50:19 +0800

    加入專案檔案。

---
+++ 20230315題目4停車費計算/20230315題目4停車費計算.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>  
+using namespace std;
+
+int main()
+{
+    int startH = 0, startM = 0, endH = 0, endM = 0;
+
+    cin >> startH >> startM;
+    cin >> endH >> endM;
+    int time = (endH * 60 + endM) - (startH * 60 + startM);
+    if (time <= 120 && time >= 0) {
+        cout << time / 30 * 30 << endl;
+    }
+    else if (time > 120 && time <= 240) {
+        cout << (time - 120) / 30 * 40 + 120 << endl;
+    }
+    else {
+        cout << (time - 240) / 30 * 60 + 120 + 160 << endl;
+    }
+    return 0;
+}
